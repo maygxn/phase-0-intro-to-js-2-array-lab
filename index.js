916f7094dba5ca416eb94bf9aaad1018d807930f
Meagan <MeaganTruong1@yahoo.com>
2023-10-10 04:02:50 -0500

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,34 @@
 // Write your solution here!
+const cats = ["Milo", "Otis", "Garfield"]; 
+function destructivelyAppendCat(name) {
+    cats.push(name);
+}
+function destructivelyPrependCat(name) {
+    cats.unshift(name);
+}
+function destructivelyRemoveLastCat() {
+    cats.pop(name);
+}
+function destructivelyRemoveFirstCat() {
+    cats.shift(name);
+}
+function appendCat(name) {
+    const copyOfCats = [...cats];
+    copyOfCats.push(name);
+    return copyOfCats;
+}
+function prependCat(name) {
+    const prependCats = [...cats];
+    prependCats.unshift(name);
+    return prependCats;
+}
+function removeLastCat() {
+    const removeCats = [...cats];
+    removeCats.pop();
+    return removeCats;
+}
+function removeFirstCat() {
+    const minusCats = [...cats];
+    minusCats.shift();
+    return minusCats;
+}
